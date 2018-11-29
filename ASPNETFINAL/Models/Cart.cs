408a6d9ce9ff9bfbe27c9223058856057b6a54df
alexaslakson <45212105+alexaslakson@users.noreply.github.com>
2018-11-29 13:50:37 -0600

    Added Cart.cs

---
+++ ASPNETFINAL/Models/Cart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETFINAL.Models
+{
+    [Table("Cart")]
+    public class Cart
+    {
+
+        public int CartId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int GameId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal Subtotal { get; set; }
+
+    }
+}
