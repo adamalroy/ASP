701e3b917b3a3cba8c185c1de7ffcc089fc3aca7
alexaslakson <45212105+alexaslakson@users.noreply.github.com>
2018-12-04 13:15:28 -0600

    added_Image.cs

---
+++ ASPNETFINAL/Migrations/201812041906011_added_Image_Class.cs
@@ -0,0 +1,16 @@
+namespace ASPNETFINAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class added_Image_Class : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
