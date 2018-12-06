fdd325f4841940956637eb65807b4a63686185be
alexaslakson <45212105+alexaslakson@users.noreply.github.com>
2018-12-06 12:51:26 -0600

    Updated Database with UserRole

---
+++ ASPNETFINAL/Migrations/201812042051434_tryTwo.cs
@@ -0,0 +1,18 @@
+namespace ASPNETFINAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class tryTwo : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "UserRole", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "UserRole");
+        }
+    }
+}
